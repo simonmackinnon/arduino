eec1f9346ffa816d3f98ba14507aa6cde27036a0
Simon <simonmackinnon@users.noreply.github.com>
2014-11-27 09:34:05 +1100

    Create lcd_tes.ino

---
+++ lcd_tes.ino
@@ -0,0 +1,47 @@
+#include <LiquidCrystal.h>
+
+#define HEALTH_LED 13
+
+LiquidCrystal lcd(4,5,6,7,8,9);
+
+void setup()
+{
+ pinMode(HEALTH_LED, OUTPUT);
+ lcd.begin(16,2);
+ lcd.clear(); 
+}
+
+void PrintStringToLCD(String strText, int xPos, int yPos)
+{
+  lcd.setCursor(yPos, xPos);
+  lcd.print(strText);
+}
+
+void PrintIntToLCD(int nNum, int nPosX, int nPosY)
+{
+  lcd.setCursor(nPosY, nPosX);
+  lcd.print(nNum);
+} 
+
+void loop()
+{
+  PrintStringToLCD("Hello World", 0, 0);
+  PrintIntToLCD(12345, 1, 0);
+  
+  digitalWrite(HEALTH_LED, HIGH);
+  delay(1000);
+  digitalWrite(HEALTH_LED, LOW);
+  delay(1000);
+  digitalWrite(HEALTH_LED, HIGH);
+  delay(1000);
+  digitalWrite(HEALTH_LED, LOW);
+  delay(1000);
+  digitalWrite(HEALTH_LED, HIGH);
+  delay(1000);
+  digitalWrite(HEALTH_LED, LOW);
+  delay(1000);
+  digitalWrite(HEALTH_LED, HIGH);
+  delay(1000);
+  digitalWrite(HEALTH_LED, LOW);
+  delay(1000);
+}
